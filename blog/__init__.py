17c290e44756d9353c47c0d0585b644b13879b57
Stefan Scorse <stefan.scorse.92@gmail.com>
2022-03-25 12:12:25 +1100

    added env file locally and requirements text file

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'adawdaw12312321'
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.sqlite'
 db = SQLAlchemy(app)
 
